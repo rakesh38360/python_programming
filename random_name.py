36f7903a67e6236aeb6db1fe0c31f2ad12fd832d
rakesh <rakesh_carpenter@epam.com>
2024-06-19 20:49:22 +0530

    commit

--- random_name.py
+++ random_name.py
@@ -2,7 +2,7 @@
 # Another developer has written the code to import the names in the inputs
 # You can run the code to see what this names list looks like.
 # Then change the names in the input to see how it imports the names.
-
+names = ["Rakesh", "Beena", "Kicchi", "Balubla"]
 # 🚨 Remember to remove the print statement above when you submit.
 import random
 start = 0
